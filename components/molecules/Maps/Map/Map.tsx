e151f5c9cdaed3744fce308e0e9a0ae6570085e5
arthaud-proust <arthaudp33@gmail.com>
2023-05-24 14:06:55 +0200

    feat: add types

--- components/molecules/Maps/Map/Map.tsx
+++ components/molecules/Maps/Map/Map.tsx
@@ -1,7 +1,7 @@
 import React, {HTMLAttributes, ReactNode, useEffect, useRef, useState} from "react"
-import * as ol from "ol";
-import MapContext from "@/components/molecules/Maps/Map/MapContext";
+import {Map as OlMap, View} from "ol";
 import {Coordinate} from "ol/coordinate";
+import MapContext from "@/components/molecules/Maps/Map/MapContext";
 
 type MapProps = {
     children: ReactNode;
@@ -11,17 +11,17 @@ type MapProps = {
 
 export default function Map({children, zoom, center, className = '', ...props}: MapProps) {
     const mapRef = useRef();
-    const [map, setMap] = useState(null);
+    const [map, setMap] = useState<OlMap | null>(null);
 
     // on component mount
     useEffect(() => {
         let options = {
-            view: new ol.View({zoom, center}),
+            view: new View({zoom, center}),
             layers: [],
             controls: [],
             overlays: []
         };
-        let mapObject = new ol.Map(options);
+        let mapObject = new OlMap(options);
         mapObject.setTarget(mapRef.current);
         setMap(mapObject);
         return () => mapObject.setTarget(undefined);
--- components/molecules/Maps/Map/MapContext.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const MapContext = new React.createContext(null);
-
-export default MapContext;
