cbe8a89bb5baa474946f9ccb2d16a7bfb29fa051
Arthaud <arthaudp33@gmail.com>
2023-05-29 18:03:50 +0200

    feat: go to login page if invalid token

--- src/redux/services/query.ts
+++ src/redux/services/query.ts
@@ -1,20 +1,23 @@
-import type {BaseQueryFn, FetchArgs, FetchBaseQueryError,} from '@reduxjs/toolkit/query'
-import {fetchBaseQuery} from '@reduxjs/toolkit/query'
-import {invalidateToken} from "@/redux/features/cykleoTokenSlice";
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query'
+import { fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { invalidateToken } from '@/redux/features/cykleoTokenSlice'
 
 export default function fetchBaseQueryAuth(params) {
-    const baseQuery = fetchBaseQuery(params);
+  const baseQuery = fetchBaseQuery(params)
 
-    const baseQueryWithReauth: BaseQueryFn<string | FetchArgs,
-        unknown,
-        FetchBaseQueryError> = async (args, api, extraOptions) => {
-        let result = await baseQuery(args, api, extraOptions)
-        if (result.error && result.error.status === 401) {
-            api.dispatch(invalidateToken());
-            location.reload();
-        }
-        return result
+  const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+    args,
+    api,
+    extraOptions
+  ) => {
+    let result = await baseQuery(args, api, extraOptions)
+    if (result.error && result.error.status === 401) {
+      api.dispatch(invalidateToken())
+
+      location.href = '/login'
     }
+    return result
+  }
 
-    return baseQueryWithReauth;
+  return baseQueryWithReauth
 }
