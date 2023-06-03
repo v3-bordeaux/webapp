f15e8984edfc1c3c02bce205ff27fd88d3ead6c7
Arthaud <arthaudp33@gmail.com>
2023-06-03 18:48:14 +0200

    feat: display station data

--- src/components/molecules/Maps/Controls/StationDetails.tsx
+++ src/components/molecules/Maps/Controls/StationDetails.tsx
@@ -2,6 +2,8 @@ import {useContext, useEffect, useState} from "react";
 import MapContext, {MapContextContent} from "@/components/molecules/Maps/Map/MapContext";
 import { Feature } from "ol";
 import type { Station } from '@/_types/tbm/ws/station'
+import { H2 } from "@/components/atoms";
+import { Bicycle, ProductHuntFill } from "akar-icons";
 
 export function StationDetails() {
     const {map} = useContext<MapContextContent>(MapContext);
@@ -42,9 +44,26 @@ export function StationDetails() {
         <section className="z-50 fixed bottom-0 left-0 w-full h-full">
             <div className="absolute inset-0 bg-backdrop-2/50" onClick={closeBottomSheet}></div>
 
-            <section className="z-50 rounded-t-2xl fixed bottom-0 left-0 w-full h-2/3 bg-background-3">
+            <article className="z-50 container py-4 rounded-t-2xl fixed bottom-0 left-0 w-full h-2/3 bg-background-3">
+                <H2 className="mb-4">{station.name}</H2>
 
-            </section>
+                <div className="flex gap-8">
+                    <div className="flex items-center gap-2">
+                        <Bicycle className="h-8 w-8"/>
+                        <span>{station.nbBikeAvailable}</span>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <Bicycle className="h-8 w-8"/>
+                        <span>{station.nbElectricBikeAvailable}</span>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <ProductHuntFill className="h-8 w-8"/>
+                        <span>{station.nbPlaceAvailable}</span>
+                    </div>
+                </div>
+            </article>
         </section>
     );
 }
