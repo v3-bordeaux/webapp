e338f21cd4f6efd49fe659c5c1bad1cb240c9f3c
Arthaud <arthaudp33@gmail.com>
2023-06-04 22:39:51 +0200

    wiip

--- src/components/molecules/Maps/Controls/CenterMapOnPosition.tsx
+++ src/components/molecules/Maps/Controls/CenterMapOnPosition.tsx
@@ -1,11 +1,16 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import MapContext, { MapContextContent } from '@/components/molecules/Maps/Map/MapContext'
 import { Location } from '@v3-bordeaux/akar-icons'
 import { useGeolocation } from '@/hooks/useGeolocation'
+import { BottomSheet } from '../../BottomSheet'
+import { H2 } from '@/components/atoms'
 
 export function CenterMapOnPosition() {
   const { map } = useContext<MapContextContent>(MapContext)
   const { geolocation, setProjection } = useGeolocation()
+  const [isAskPermissionBottomSheetOpen, setIsAskPermissionBottomSheetOpen] = useState(false)
+
+  const canGeolocate = !!map && !!geolocation && !!geolocation.getPosition()
 
   useEffect(() => {
     if (!map) {
@@ -20,16 +25,34 @@ export function CenterMapOnPosition() {
       return
     }
 
+    if (!geolocation.getPosition()) {
+      setIsAskPermissionBottomSheetOpen(true)
+      return
+    }
+
     map.getView().setCenter(geolocation.getPosition())
   }
 
   return (
-    <button
-      className="pointer-events-auto rounded-full p-3 bg-cta-1 border-2 border-text-1 shadow-brut active:shadow-none active:translate-x-1 active:translate-y-1"
-      onClick={centerMapOnPosition}
-      disabled={!map || !geolocation}
-    >
-      <Location className="h-8 w-8" />
-    </button>
+    <>
+      <button
+        className={`pointer-events-auto rounded-full p-3 border-2 border-text-1 bg-cta-1 ${
+          canGeolocate
+            ? ' shadow-brut active:shadow-none active:translate-x-1 active:translate-y-1'
+            : ' grayscale'
+        }`}
+        onClick={centerMapOnPosition}
+        disabled={canGeolocate}
+      >
+        <Location className="h-8 w-8" />
+      </button>
+
+      <BottomSheet
+        isOpen={isAskPermissionBottomSheetOpen}
+        onClick={() => setIsAskPermissionBottomSheetOpen(false)}
+      >
+        <H2>Vous devez autoriser la localisation</H2>
+      </BottomSheet>
+    </>
   )
 }
